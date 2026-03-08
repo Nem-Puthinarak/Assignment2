c1b1cee06eac3e74c013bb176b9146882ed7ff0e
Narak <nrraiyugin@gmail.co>
2026-03-08 17:36:16 +0700

    new api

--- Jenkin/index.js
+++ Jenkin/index.js
@@ -27,6 +27,11 @@ app.delete('/del', (req, res) => {
     res.send('<html><body>INSIDE DELETE API..</body></html>');
 });
 
+// STATUS API
+app.get('/status', (req, res) => {
+    res.send('<html><body>Server is running fine </body></html>');
+});
+
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
 app.listen(5000, () =>
     console.log('EXPRESS Server Started at Port No: 5000'));
